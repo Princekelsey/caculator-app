b51d1473bf120d1b2e46a9de7c7fc6f70adf84b3
Prince Ajuobi <princekelsey299@gmail.com>
2020-04-20 23:05:03 +0300

    redesigned calculator UI, added new components and calculator class

--- App.js
+++ App.js
@@ -1,256 +1,42 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import Inputs from './components/Inputs';
+import React, {Component} from 'react';
+import Screen from './components/Screen';
+import {View, StyleSheet} from 'react-native';
+import Keybord from './components/Keybord';
 
-const App = () => {
-  const [caluculatorState, setState] = useState({
-    displayValue: '',
-    operator: null,
-    result: 0,
-  });
+const Calculator = require('./logic');
 
-  const inputButtons = [
-    ['CLEAR', 'KM', 'MILES', 'DEL'],
-    [7, 8, 9, '÷'],
-    [4, 5, 6, '×'],
-    [1, 2, 3, '-'],
-    [0, '.', '=', '+'],
-  ];
-
-  const {displayValue, operator, result} = caluculatorState;
-
-  const calculateInput = () => {
-    const input = displayValue;
-
-    const calculated = eval(input);
-
-    setState({
-      ...caluculatorState,
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       displayValue: '',
-      result: calculated % 1 === 0 ? calculated : calculated.toFixed(2),
+      result: 0,
+    };
+
+    //initialize calculator
+    this.calculator = new Calculator();
+  }
+  handleClick = (keyLog, math) => {
+    this.calculator.keyClick(keyLog, math);
+    this.setState({
+      displayValue: this.calculator.mainDisplay(),
+      result: this.calculator.calculatedResult(),
     });
   };
-
-  const handlePress = (value) => {
-    switch (value) {
-      case 'DEL':
-        if (displayValue === '') return;
-        const text = displayValue.split('');
-        text.pop();
-        setState({
-          ...caluculatorState,
-          displayValue: text.join(''),
-        });
-        break;
-      case '+':
-      case '×':
-      case '-':
-      case '÷':
-        const lastChar = displayValue.split('').pop();
-        if (
-          displayValue === '' ||
-          (operator === value && operator === lastChar) ||
-          lastChar === '+' ||
-          lastChar === '*' ||
-          lastChar === '-' ||
-          lastChar === '/'
-        )
-          return;
-        let ope;
-        if (value === '÷') {
-          ope = '/';
-        } else if (value === '×') {
-          ope = '*';
-        } else {
-          ope = value;
-        }
-        setState({
-          ...caluculatorState,
-          nextValue: true,
-          operator: value,
-          displayValue: displayValue + ope,
-        });
-        break;
-      case '=':
-        const last = displayValue.split('').pop();
-        if (
-          displayValue === '' ||
-          last === '+' ||
-          last === '*' ||
-          last === '-' ||
-          last === '/'
-        )
-          return;
-        calculateInput();
-        break;
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 0:
-        setState({
-          ...caluculatorState,
-          displayValue: displayValue + value,
-        });
-
-        break;
-      case 'MILES':
-        const lastValue = displayValue.split('').pop();
-
-        if (
-          (displayValue === '' && result === 0) ||
-          lastValue === '+' ||
-          lastValue === '*' ||
-          lastValue === '-' ||
-          lastValue === '/'
-        )
-          return;
-        if (result !== 0) {
-          const miles = result / 1.60934;
-          setState({
-            ...caluculatorState,
-            displayValue: result.toString() + 'km' + ' ' + 'to miles',
-            result: miles % 1 === 0 ? miles : miles.toFixed(2),
-          });
-          return;
-        }
-        if (
-          lastValue === '+' ||
-          lastValue === '*' ||
-          lastValue === '-' ||
-          lastValue === '/'
-        )
-          return;
-
-        const miles = displayValue / 1.60934;
-        setState({
-          ...caluculatorState,
-          displayValue: displayValue + 'km' + ' ' + 'to miles',
-          result: miles % 1 === 0 ? miles : miles.toFixed(2),
-        });
-
-        return;
-      case 'KM':
-        const lastC = displayValue.split('').pop();
-
-        if (
-          (displayValue === '' && result === 0) ||
-          lastC === '+' ||
-          lastC === '*' ||
-          lastC === '-' ||
-          lastC === '/'
-        )
-          return;
-        if (result !== 0) {
-          const km = result * 1.60934;
-          setState({
-            ...caluculatorState,
-            displayValue: result.toString() + 'miles' + ' ' + 'to km',
-            result: km % 1 === 0 ? km : km.toFixed(2),
-          });
-          return;
-        }
-        if (lastC === '+' || lastC === '*' || lastC === '-' || lastC === '/')
-          return;
-
-        const km = displayValue * 1.60934;
-        setState({
-          ...caluculatorState,
-          displayValue: displayValue + 'miles' + ' ' + 'to Km',
-          result: km % 1 === 0 ? km : km.toFixed(2),
-        });
-
-        return;
-
-      case '.':
-        const dot = displayValue.toString().slice(-1);
-
-        setState({
-          ...caluculatorState,
-          displayValue: dot !== '.' ? displayValue + value : displayValue,
-        });
-
-        break;
-      default:
-        setState({
-          displayValue: '',
-          result: 0,
-          operator: null,
-        });
-        break;
-    }
-  };
-
-  const renderInputButtons = () => {
-    const layout = inputButtons.map((button, i) => {
-      const btnRow = button.map((item, i) => (
-        <Inputs value={item} key={item} handlePress={handlePress} />
-      ));
-      return (
-        <View style={styles.btnRow} key={i}>
-          {btnRow}
-        </View>
-      );
-    });
-    return layout;
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.resultContainer}>
-        <Text style={styles.input}>{displayValue}</Text>
+  render() {
+    const {displayValue, result} = this.state;
+    return (
+      <View style={styles.conatiner}>
+        <Screen result={result} log={displayValue} />
+        <Keybord keyClick={this.handleClick} />
       </View>
-      <View style={styles.caululationContainer}>
-        <Text style={styles.result}>{result}</Text>
-      </View>
-      <View style={styles.inputContainer}>{renderInputButtons()}</View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
+  conatiner: {
     flex: 1,
-  },
-  resultContainer: {
-    flex: 2,
-    justifyContent: 'center',
-    backgroundColor: '#D3D3D3',
-  },
-
-  caululationContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#D3D3D3',
-  },
-  result: {
-    fontSize: 60,
-    fontWeight: 'bold',
-    paddingRight: 10,
-    textAlign: 'right',
-  },
-
-  inputContainer: {
-    flex: 8,
-    backgroundColor: '#3d0075',
-  },
-  input: {
-    color: 'black',
-    fontSize: 30,
-    paddingTop: 20,
-    paddingRight: 10,
-    textAlign: 'right',
-    // fontWeight: 'lighter',
-  },
-  btnRow: {
-    flex: 1,
-    flexDirection: 'row',
+    backgroundColor: '#4b5568',
   },
 });
-
-export default App;
---
+++ components/Key.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+
+export default class Key extends Component {
+  render() {
+    return (
+      <View>
+        <TouchableOpacity
+          onPress={() =>
+            this.props.keyClick(this.props.keyLog, this.props.math)
+          }
+          style={[styles.container, this.props.style]}>
+          <Text style={styles.text}>{this.props.Tag}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {alignItems: 'center', justifyContent: 'center'},
+  text: {fontSize: 20, fontWeight: 'bold', color: '#fff'},
+});
